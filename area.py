08674834409a9aa9d0fb5a48119b6173a335ba8a
NIVEDH SUNIL <nishikradha@gmail.com>
2026-08-06 18:32:49 +0530

    area

---
+++ area.py
@@ -0,0 +1,7 @@
+import math
+def circle_area(radius):
+    area = math.pi * radius ** 2
+    return area
+def rectangle_area(width, height):
+    area = width * height
+    return area
---
+++ using python module for area.py
@@ -0,0 +1,3 @@
+import area
+print("area of circle: ", area.circle_area(10))
+print("area of rectangle: ", area.rectangle_area(10,5))
